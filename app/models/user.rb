f649d786c52b3bd4bf7042c470790ab2656bb3c8
Salomanuel <salomanuel.aux+code@gmail.com>
2018-03-20 16:58:52 +0100

    add email lowercase conversion before save

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
 
+  before_save { self.email = email.downcase }
+
   VALID_EMAIL_REGEX = /\A[\d\+\.a-z_-]+@[a-z]+\.[a-z.]+\z/i
   validates(:username,  presence:   true, 
                         length:     { maximum: 50 } )
