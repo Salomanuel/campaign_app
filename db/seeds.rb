8e1c4bffc44b7061c5a1e5f598a9beeea5fe6893
Salomanuel <salomanuel.aux+code@gmail.com>
2018-03-20 21:47:30 +0100

    implement status

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,24 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+
+
 99.times do |n|
   name    = Faker::Pokemon.name
   email   = "example-#{n}@manuel.uoo"
   expert  = rand(2).zero?
+
+  case rand(3)
+  when 0 then status = true
+  when 1 then status = false
+  when 2 then status = nil
+  end
+
   User.create!( 
     username: name, 
     email: email,
     password: "password",
     password_confirmation: "password",
-    expert: expert)
+    expert: expert,
+    status: status)
 end
