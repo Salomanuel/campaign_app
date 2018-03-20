bd32f09ddb60f7d741c1025fb2a568550f1eca9e
Salomanuel <salomanuel.aux+code@gmail.com>
2018-03-20 19:06:07 +0100

    add tests for sessions

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,13 @@ class User < ApplicationRecord
                         length:     { maximum: 255 }, 
                         format:     { with: VALID_EMAIL_REGEX },
                         uniqueness: { case_sensitive: false }
+  has_secure_password
   validates :password,  presence:   true,
                         length:     { minimum: 6 }, 
                         allow_nil: true
-  has_secure_password                        
+  # for testing with fixtures
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end                       
 end
