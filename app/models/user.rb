6ebc21ff0ab176655c103a4ca28e3af3f2c8eb01
Salomanuel <salomanuel.aux+code@gmail.com>
2018-03-20 16:20:07 +0100

    validation tests for User pass

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+
+  VALID_EMAIL_REGEX = /\A[\d\+\.a-z_-]+@[a-z]+\.[a-z.]+\z/i
+  validates(:username,  presence:   true, 
+                        length:     { maximum: 50 } )
+  validates :email,     presence:   true, 
+                        length:     { maximum: 255 }, 
+                        format:     { with: VALID_EMAIL_REGEX },
+                        uniqueness: { case_sensitive: false }
 end
