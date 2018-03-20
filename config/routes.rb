0d1b3198aa510d98cb09db82933c734a99fc1901
Salomanuel <salomanuel.aux+code@gmail.com>
2018-03-20 16:42:30 +0100

    correct massive oversight in routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
   get 'static_pages/home'
 
 
-  get '/login',   to: 'sessions#new'
-  get '/login',   to: 'sessions#create'
-  get '/logout',  to: 'sessions#destroy'
+  get     '/login',   to: 'sessions#new'
+  post    '/login',   to: 'sessions#create'
+  delete  '/logout',  to: 'sessions#destroy'
 
 
   resources :users
