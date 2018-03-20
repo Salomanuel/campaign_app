b7ef80dbe26fcfc5ab428a922ba29744fede5e25
Salomanuel <salomanuel.aux+code@gmail.com>
2018-03-20 16:44:29 +0100

    define #create in sessions_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create
-    render 'new'
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # log user in
+    else
+      # create error message
+      render 'new'
+    end
   end
+
+  def destroy
 end
