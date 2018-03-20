fe4e95ec50f5bfa3f518c4e6089a735f0748c5d7
Salomanuel <salomanuel.aux+code@gmail.com>
2018-03-20 20:07:26 +0100

    add tests for redirection

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
+  def setup
+    @user       = users(:manuel)
+    @other_user = users(:poor_user)
+  end
+
+  # test "should redirect index when not logged in" do
+  #   get users_path
+  #   assert_redirected_to login_url
   # end
+
+  test "should get new" do
+    get signup_path
+    assert_response :success
+  end
 end
