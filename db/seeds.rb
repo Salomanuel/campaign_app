b21d2b94d74768007b9510a8a0e6fc5d7745f728
Salomanuel <salomanuel.aux+code@gmail.com>
2018-03-20 15:49:52 +0100

    add faker gem and seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+99.times do |n|
+  name    = Faker::Pokemon.name
+  email   = "example-#{name}@manuel.uoo"
+  User.create!(email: email, username: name)
+end
