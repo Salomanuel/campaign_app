24a23746f40d3437ec84192e8a770810422cd137
Salomanuel <salomanuel.aux+code@gmail.com>
2018-03-22 00:16:22 +0100

    implement tag search

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -5,5 +5,15 @@ class TagsController < ApplicationController
 
   def index
     @tags = Tag.all
+
+    if params[:submitted]
+      @search_results = find_tag(params)
+    end
   end
+
+
+  private
+    def find_tag(params)
+      Tag.find_by(name: params[:tag_name])
+    end
 end
