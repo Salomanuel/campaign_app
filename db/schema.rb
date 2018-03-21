2bdf1ce785cc9ce0dae79e760d110edca48ec0f0
Salomanuel <salomanuel.aux+code@gmail.com>
2018-03-21 17:16:06 +0100

    add Discussion to Campain

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180321161329) do
+ActiveRecord::Schema.define(version: 20180321161533) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 20180321161329) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "discussion_id"
+    t.index ["discussion_id"], name: "index_campaigns_on_discussion_id"
     t.index ["user_id", "created_at"], name: "index_campaigns_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_campaigns_on_user_id"
   end
@@ -43,5 +45,6 @@ ActiveRecord::Schema.define(version: 20180321161329) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "campaigns", "discussions"
   add_foreign_key "campaigns", "users"
 end
