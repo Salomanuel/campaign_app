18e4670a10cdb3faf25b6e8572733a09426e9816
Salomanuel <salomanuel.aux+code@gmail.com>
2018-03-21 19:39:51 +0100

    before filter login for User#show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index]
+  before_action :logged_in_user, only: [:index, :show]
   def index
     @users = User.paginate(page: params[:page])
   end
