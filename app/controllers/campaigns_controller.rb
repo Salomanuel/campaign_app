804994fd6c5557fa2f7b41c4e4dbc7becf3830d9
Salomanuel <salomanuel.aux+code@gmail.com>
2018-03-21 12:12:51 +0100

    create basic #create for Campaigns

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,5 +1,6 @@
 class CampaignsController < ApplicationController
   before_action :logged_in_user, only: [:index, :create, :destroy]
+  # add filters to campaign creation for expert users 
 
   def index
     @campaigns = Campaign.paginate(page: params[:page])
@@ -9,5 +10,17 @@ class CampaignsController < ApplicationController
   end
 
   def create
+    @campaign = current_user.campaigns.build(campaign_params)
+    if @campaign.save
+      flash[:success] = "Campaign created!"
+      redirect_to campaigns_path
+    else
+      render 'static_pages/home'
+    end
+
+    private
+      def campaign_params
+        params.require(:campaign).permit(:title)
+      end
   end
 end
