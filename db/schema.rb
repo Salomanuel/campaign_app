b920119cf30565a1ea8ba144fa4dd67f70f886f9
Salomanuel <salomanuel.aux+code@gmail.com>
2018-03-21 23:16:35 +0100

    generate Tag and Tagging models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180321212401) do
+ActiveRecord::Schema.define(version: 20180321221559) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,21 @@ ActiveRecord::Schema.define(version: 20180321212401) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "taggings", force: :cascade do |t|
+    t.bigint "tag_id"
+    t.bigint "campaign_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["campaign_id"], name: "index_taggings_on_campaign_id"
+    t.index ["tag_id"], name: "index_taggings_on_tag_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "username"
@@ -67,5 +82,7 @@ ActiveRecord::Schema.define(version: 20180321212401) do
   add_foreign_key "comments", "campaigns"
   add_foreign_key "comments", "discussions"
   add_foreign_key "comments", "users"
+  add_foreign_key "taggings", "campaigns"
+  add_foreign_key "taggings", "tags"
   add_foreign_key "users", "comments"
 end
