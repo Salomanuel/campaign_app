b6c9f74ba8812791a5ffe1261684dbe551e407b5
Salomanuel <salomanuel.aux+code@gmail.com>
2018-03-21 11:56:02 +0100

    add filters for logged in users to campaign

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,17 @@
 class ApplicationController < ActionController::Base
+  # for CSRF protection
   protect_from_forgery with: :exception
 
   include SessionsHelper
+
+  private
+
+    # confirms a logged-in user
+    def logged_in_user
+      unless logged_in?
+        # store_location # could implement that
+        flash[:danger] = "Please log in"
+        redirect_to login_url
+      end
+    end
 end
--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,4 +1,5 @@
 class CampaignsController < ApplicationController
+  before_action :logged_in_user, only: [:index, :create, :destroy]
 
   def index
     @campaigns = Campaign.paginate(page: params[:page])
