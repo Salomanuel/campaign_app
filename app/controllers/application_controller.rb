dd8fee6e00e874c953071007e5a561b82b16cc09
Salomanuel <salomanuel.aux+code@gmail.com>
2018-03-21 13:39:25 +0100

    fix small bug of a wrong redirect in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
     def logged_in_user
       unless logged_in?
         flash[:danger] = "Please log in"
-        redirect_to current_user
+        redirect_to login_url
       end
     end
 end
