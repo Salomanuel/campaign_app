58b395ed6e0618a18febccd39d734ec1f66095cc
Salomanuel <salomanuel.aux+code@gmail.com>
2018-03-21 11:50:11 +0100

    add tests with fixtures for campaigns actions

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -3,4 +3,10 @@ class CampaignsController < ApplicationController
   def index
     @campaigns = Campaign.paginate(page: params[:page])
   end
+
+  def destroy
+  end
+
+  def create
+  end
 end
--- test/controllers/campaigns_controller_test.rb
+++ test/controllers/campaigns_controller_test.rb
@@ -1,7 +1,27 @@
 require "test_helper"
 
 describe CampaignsController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+# #   # it "must be a real test" do
+# #   #   flunk "Need real tests"
+# #   # end
+# # end
+# class CampaignsControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @campaign = campaigns(:one)
+  end
+
+  test "should redirect create when not logged in" do
+    assert_no_difference "Campaign.count" do
+      post campaigns_path, params: { campaign: { content: "Lorem" } }
+    end
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when not logged in" do
+    assert_no_difference "Campaign.count" do
+      delete campaign_path(@campaign)
+    end
+    assert_redirected_to login_url
+  end
 end
